06db2505836f8d650776e059bd5e811287b05a69
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-08 15:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday,name){
+  return `happy ${holiday}, ${name}`
+}
+
+function holidayCountdown(holiday,days){
+  return `Happy ${holiday}, ${name}`
+}
